e2c9806f8772c828f9591096eb7f6e6431b55dd7
Devon <devurauxe@gmail.com>
2019-11-14 02:49:32 -0500

    Sheeb Controller sheebed

--- Sheeb Game/Assets/Scripts/Sheep_Controller.cs
+++ Sheeb Game/Assets/Scripts/Sheep_Controller.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Sheep_Controller : MonoBehaviour
 {
-    internal Collider2D sheeb_Collider;
-    internal Rigidbody2D rb;
+    internal Collider2D sheeb_Collider; // The sheeb's individual collider
+    internal Rigidbody2D rb; // The rigidbody of the sheeb (not used currently)
 
-    public Transform target;
+    internal bool keep_Moving = true; // Should the sheeb keep moving?
 
-    internal bool keep_Moving = true;
-    internal bool in_Area;
-
-    public Collider2D SheebCollider { get { return sheeb_Collider; } }
+    public Collider2D SheebCollider { get { return sheeb_Collider; } } // Get method for sheeb collider
 
     // Start is called before the first frame update
     void Start()
     {
+        // Randomly determine the sheeb's tag and color
         switch (Random.Range(0, 3))
         {
             case 0:
@@ -38,6 +36,7 @@ public class Sheep_Controller : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    // Move the sheeb forward
     public void Move(Vector2 velocity)
     {
         if (keep_Moving)
@@ -49,17 +48,8 @@ public class Sheep_Controller : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Set the sheeb's to ignore the collision between other sheebs
         if (collision.gameObject.tag.Contains("Sheeb"))
             Physics2D.IgnoreCollision(sheeb_Collider, collision.collider);
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag.Contains("Sheeb_Point"))
-            keep_Moving = true;
     }
 }
