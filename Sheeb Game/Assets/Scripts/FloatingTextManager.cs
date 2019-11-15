4b9ecf7870357cb8c2c5fe65ce12e07df66e4d53
DanNorwayMan <57687542+DanNorwayMan@users.noreply.github.com>
2019-11-15 04:02:05 -0500

    There is a score UI in Dan Scene
    It is being activated by pressing return, but whoever programs the grass can find a comment in the code where they should replace the activation method.

---
+++ Sheeb Game/Assets/Scripts/FloatingTextManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingTextManager : MonoBehaviour
+{
+    private static FloatingTextManager instance;
+
+    public GameObject textPrefab;
+
+    public Transform textSpawn;
+
+    public RectTransform canvasTransform;
+
+    public static FloatingTextManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = GameObject.FindObjectOfType<FloatingTextManager>();
+            }
+            return instance;
+        }
+    }
+
+    public void CreateText()
+    {
+        GameObject ftm = Instantiate(textPrefab, textSpawn.position, textSpawn.rotation);
+
+        ftm.transform.SetParent(canvasTransform);
+        ftm.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+    }
+
+}
